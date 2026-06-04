61f163395eb29b26b83a14907a063e8095a5d75c
Dored <leylatou68@gmail.com>
2026-06-04 19:27:18 +0000

    fonction de gestion des produits

--- service/Admin.php
+++ service/Admin.php
@@ -1,6 +1,6 @@
 <?php 
     //ajouter admin
-    function creerAdmin($nom,$prenom,$tel,$redirect,? int  $password = null){
+    function creerAdmin($nom, $prenom, $tel, $redirect, ?int $password = null){
         $nom = htmlspecialchars($nom);
         $prenom = htmlspecialchars($prenom);
         $tel = htmlspecialchars($tel);
@@ -9,20 +9,22 @@
         $sam = $connexion->prepare("INSERT INTO admin (nomadmin,prenomadmin,teladmin,password) VALUES (?,?,?,?)");
         $sam->execute([$nom, $prenom,$tel,$password]);
         $connexion = null;
-        //header("location: $redirect");
+        header("location: $redirect");
 
     }
-        //voir tout les admins
+    
+    //voir tout les admins
     function voirAllAdmin(){
         include dirname(__DIR__). '/fonction/connexion.php';
         $sam = $connexion->prepare("SELECT * FROM admin");
         $sam->execute();
         $admins = $sam->fetchAll(PDO::FETCH_ASSOC);
-        var_dump($admins);
+        // var_dump($admins);
          $connexion = null;
     }
-       //voir un seul admin
-    function voirAdmin($id){
+
+     //voir un seul admin
+    function voirAdmin( $id ){
         $id= intval($id);
         include dirname(__DIR__). '/fonction/connexion.php';
         $sam = $connexion->prepare("SELECT * FROM admin WHERE idadmin= ?");
@@ -31,20 +33,23 @@
        // var_dump($admin);
          $connexion = null;
     }
-        //mise à jour de admin
-    function updateAdmin($nom,$prenom,$tel,$redirect,$id,? int  $password = null){
+
+    //mise à jour de admin
+    function updateAdmin($nom ,$prenom ,$tel ,$redirect ,$id ,?int $password = null){
          $id= intval($id);
          $nom = htmlspecialchars($nom);
          $prenom = htmlspecialchars($prenom);
          $tel = htmlspecialchars($tel);
          $password = htmlspecialchars($password);
          include dirname(__DIR__). '/fonction/connexion.php';
-         $sam = $connexion->prepare("UPDATE admin SET nomadmin = ?,prenomadmin=?, teladmin = ?,password =? WHERE idadmin = ? ");
+         $sam = $connexion->prepare("UPDATE admin SET nomadmin = ?,prenomadmin= ?, teladmin = ?,password =? WHERE idadmin = ? ");
          $sam->execute([$nom, $prenom,$tel,$password,$id]);
          $connexion = null;
+          //header("location: $redirect");
     }
-        //supprimer admin
-    function supprimerAdmin($id){
+
+    //supprimer admin
+    function supprimerAdmin( $id ){
          $id= intval($id);
          include dirname(__DIR__). '/fonction/connexion.php';
          $sam = $connexion->prepare("DELETE FROM admin WHERE idadmin = ?");
---
+++ service/Produit.php
@@ -0,0 +1,63 @@
+<?php
+
+    //ajouter des produits
+    function creerProduit($nom,int $stock, int $prix,string $description,$redirect,?string $image = null){
+        $nom = htmlspecialchars($nom);
+        $stock = intval($stock);
+        $prix = intval($prix);
+        $description = htmlspecialchars($description);
+        $image = htmlspecialchars($image);
+        include dirname(__DIR__). '/fonction/connexion.php';
+        $sam = $connexion->prepare("INSERT INTO produit (nomprod,stockprod,prixprod,description,imageprod)
+          VALUES (?,?,?,?,?)");
+        $sam->execute([$nom,$stock,$prix,$description,$image]);
+        $connexion = null;
+         header("location: $redirect");
+    }
+
+   //voir tout les produits
+    function voirAllProduits(){
+        include dirname(__DIR__). '/fonction/connexion.php';
+        $sam = $connexion->prepare("SELECT * FROM produit");
+        $sam->execute();
+        $Produits = $sam->fetchAll(PDO::FETCH_ASSOC);
+        // var_dump( $Produits);
+        $connexion = null;
+    }
+
+         //voir un seul produit
+    function voirProduit( $id ){
+        $id= intval($id);
+        include dirname(__DIR__). '/fonction/connexion.php';
+        $sam = $connexion->prepare("SELECT * FROM produit WHERE idprod= ?");
+        $sam->execute([$id]);
+        $produit= $sam->fetch(PDO::FETCH_ASSOC);
+        // var_dump($produit);
+         $connexion = null;
+    }
+     //mise à jour de produits
+    function updateProduit($nom,int $stock, int $prix,string $description,$redirect,$id,?string $image = null){
+        $id= intval($id);
+        $nom = htmlspecialchars($nom);
+        $stock = intval($stock);
+        $prix = intval($prix);
+        $description = htmlspecialchars($description);
+        $image = htmlspecialchars($image);
+        include dirname(__DIR__). '/fonction/connexion.php';
+        $sam = $connexion->prepare("UPDATE produit SET nomprod = ?,stockprod = ?,prixprod = ?,description =? ,imageprod =? WHERE idprod = ? ");
+        $sam->execute([$nom,$stock,$prix,$description,$image,$id]);
+        $connexion = null;
+         header("location: $redirect");
+    }
+     
+    //supprimer produit
+    function supprimerproduit( $id ){
+         $id= intval($id);
+         include dirname(__DIR__). '/fonction/connexion.php';
+         $sam = $connexion->prepare("DELETE FROM produit WHERE idprod = ?");
+         $sam->execute([$id]);
+         $connexion = null;
+     }    
+    
+    
+?>
